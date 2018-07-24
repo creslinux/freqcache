e6c8d658d487c29a43885730cf10888300067ee7
creslinux <creslin@gmail.com>
2018-07-24 14:46:44 +0000

    Added SNI to hitch

--- 3_hitch/start.sh
+++ 3_hitch/start.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+HITCH_PEM=""
+for x in ` find /etc/ssl/hitch -type f -name "*.pem" ;`
+do
+ HITCH_PEM="$HITCH_PEM ${x}"
+done
+echo ${HITCH_PEM}
+
 exec bash -c \
   "exec /usr/local/sbin/hitch --user=hitch \
   $HITCH_BACKEND \
