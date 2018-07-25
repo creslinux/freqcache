ebb0b73af5cf24f1a55008ba02f257b8cab27e73
creslinux <creslin@gmail.com>
2018-07-25 08:24:35 +0000

    Added 7_unbound into reqcache suite

---
+++ 7_unbound/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+exec unbound -d -d
--- uninstall.sh
+++ uninstall.sh
@@ -2,9 +2,9 @@
 #/usr/bin/env bash
 ## Clear down freqcache containers, images, generated content 
 
-docker stop ft_stunnel ft_varnish ft_hitch ft_api_admin ft_ca ft_dnsmask
-docker rm ft_stunnel ft_varnish ft_hitch ft_api_admin ft_ca ft_dnsmasq
-docker rmi freqcache_ft_stunnel freqcache_ft_varnish freqcache_ft_hitch freqcache_ft_api_admin freqcache_ft_ca freqcache_ft_dnsmasq
+docker stop ft_stunnel ft_varnish ft_hitch ft_api_admin ft_ca ft_dnsmask ft_unbound
+docker rm ft_stunnel ft_varnish ft_hitch ft_api_admin ft_ca ft_dnsmasq ft_unbound
+docker rmi freqcache_ft_stunnel freqcache_ft_varnish freqcache_ft_hitch freqcache_ft_api_admin freqcache_ft_ca freqcache_ft_dnsmasq freqcache_ft_unbound
 
 # Remove old certs and vcl configs
 rm -rf 5_ca/ca
