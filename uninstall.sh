d4a354c13ee84c7fb0457839d5a63a647054c9d1
creslinux <creslin@gmail.com>
2018-07-25 09:01:25 +0000

    typo

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@
 #/usr/bin/env bash
 ## Clear down freqcache containers, images, generated content 
 
-docker stop ft_stunnel ft_varnish ft_hitch ft_api_admin ft_ca ft_dnsmask ft_unbound
+docker stop ft_stunnel ft_varnish ft_hitch ft_api_admin ft_ca ft_dnsmasq ft_unbound
 docker rm ft_stunnel ft_varnish ft_hitch ft_api_admin ft_ca ft_dnsmasq ft_unbound
 docker rmi freqcache_ft_stunnel freqcache_ft_varnish freqcache_ft_hitch freqcache_ft_api_admin freqcache_ft_ca freqcache_ft_dnsmasq freqcache_ft_unbound
 
