9d2ba291e4fa09863bc273c69f27b63fa54cd4de
creslin <34645187+creslinux@users.noreply.github.com>
2018-07-25 16:43:28 +0000

    Update varnish_backends.sh

--- 5_ca/varnish_backends.sh
+++ 5_ca/varnish_backends.sh
@@ -13,10 +13,6 @@ here=${str%/*}
 cd ${here}
 
 #### Example output - include
-#if (req.http.host == "www.myhost2.com") {
-#    set req.backend = vm2;
-#}
-#### Example output - backend 
 #backend vm1 {
 #    .host = "10.0.0.11";
 #    .port = "80";
@@ -24,9 +20,17 @@ cd ${here}
 #    .first_byte_timeout = 6000s;
 #    .between_bytes_timeout = 6000s;
 #}
+#######
+#if (req.http.host == "www.myhost2.com") {
+#    set req.backend = vm2;
+#}
+#}
 
 port=50000 
->../2_varnish/etc/varnish/backends.vcl
+# Empty the files
+rm ../2_varnish/etc/varnish/backends.vcl
+rm ../2_varnish/etc/varnish/b_end
+rm ../2_varnish/etc/varnish/b_inc
 
 for x in `cat ../5_ca/api-list | tr '[:upper:]' '[:lower:]'| grep [a-z]` 
 do
@@ -36,9 +40,11 @@ if (req.http.host == "\"${x}\"") {
     set req.backend = ${x};
 }
 " 
+# no "." in backend names, replace with _
+y=`echo ${x} | sed 's/\./_/g'`
 
 CONFIG_BACK="
-backend ${x} {
+backend ${y} {
     .host = "\"ft_stunnel\"";
     .port = "\"${port}\"";
     .connect_timeout = 60s;
@@ -48,10 +54,14 @@ backend ${x} {
 "
 port=$((port + 1))
 
-echo "${CONFIG_INC}">>../2_varnish/etc/varnish/backends.vcl
-echo "${CONFIG_BACK}">>../2_varnish/etc/varnish/backends.vcl
+echo "${CONFIG_BACK}">>../2_varnish/etc/varnish/b_end
+echo "${CONFIG_INC}">>../2_varnish/etc/varnish/b_inc
 
 done
+
+# fill backends.vcl - backends then includes
+cat ../2_varnish/etc/varnish/b_end>>../2_varnish/etc/varnish/backends.vcl
+cat ../2_varnish/etc/varnish/b_inc>>../2_varnish/etc/varnish/backends.vcl
 
 # Send back 
 cd ..
