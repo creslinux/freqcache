361fcf8e681de2fd573587123c40e30121df4119
creslin <34645187+creslinux@users.noreply.github.com>
2018-07-25 16:00:23 +0000

    Update client.sh

--- 1_stunnel/client.sh
+++ 1_stunnel/client.sh
@@ -1,22 +1,3 @@
 #!/usr/bin/env bash
 
-#Start dnsmaq 
-service dnsmasq start
-
-#Build config for stunnel
-CONFIG="
-verify = 2
-CAfile = /etc/ssl/certs/ca-certificates.crt
-sslVersion = all
-
-foreground = yes
-
-[binance]
-client = yes
-verify = 0
-accept = 8080
-connect = api.binance.com:443
-"
-
-echo "$CONFIG" > /etc/stunnel/stunnel.conf
 /usr/bin/stunnel
