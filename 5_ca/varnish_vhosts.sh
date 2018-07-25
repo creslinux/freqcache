e5484286c348eaaaa73daa65d105218c5c60540f
creslinux <creslin@gmail.com>
2018-07-25 11:59:33 +0000

    fixed var in sed not wrting ap target names

--- 5_ca/varnish_vhosts.sh
+++ 5_ca/varnish_vhosts.sh
@@ -8,7 +8,7 @@
 ########################
 
 ######
-# Glue exection to its own directory 
+# Glue exection to its own directory
 ##
 str="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 here=${str%/*}
@@ -19,28 +19,28 @@ cd ${here}
 ##
 
 # Empty the file
->../2_varnish/etc/varnish/api-targets.vcl 
+>../2_varnish/etc/varnish/api-targets.vcl
 
 # Append entries
 for x in `cat api-list`
-do 
-	y="include \"sites-enabled/${x}.vcl\";" 
+do
+	y="include \"sites-enabled/${x}.vcl\";"
 	echo ${y} >>../2_varnish/etc/varnish/api-targets.vcl
 done
 cat ../2_varnish/etc/varnish/api-targets.vcl
 echo "includes added to 2_varnish/etc/varnish/api-targets.vcl"
 
 #####
-# 
-# Create cache rules for each URL in api-list   
+#
+# Create cache rules for each URL in api-list
 # copy template and substitute the domains host within
 #
 ##
 rm ../2_varnish/etc/varnish/sites-enabled/*.vcl
-for x in `cat api-list `  
-do 
+for x in `cat api-list `
+do
         cat ../2_varnish/etc/varnish/sites-enabled/aaa_template \
-        | sed -e 's/aaa_template.com/api.binance.com/g' \
+        | sed -e "s/aaa_template.com/${x}/g" \
         > ../2_varnish/etc/varnish/sites-enabled/${x}.vcl
 
 	echo "created Varnish rules: ${x}.vcl"
