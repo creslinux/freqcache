abb165e3842c36bb540c84490e7740a8a4c7624d
creslin <34645187+creslinux@users.noreply.github.com>
2018-07-31 10:19:23 +0000

    Update run.sh
    Set timeout for clients to be 330 seconds, default is 5 seconds. this was causing hitch to create a new SSL session for every client request.

--- 2_varnish/run.sh
+++ 2_varnish/run.sh
@@ -10,6 +10,9 @@ exec bash -c \
    -T localhost:6082 \
    -S /etc/varnish/secret \
    -p vcc_err_unref=off \
+   -p timeout_idle=330 \
+   -p thread_pool_min=500 \
+   -p thread_pool_max=2000 \
    -f /etc/varnish/default.vcl"
 
 while : ; do sleep 1000; done 
